a5782bf15ec42e176b1fd4b15c716a0b249a6e6d
AYUSH SRIVASTAVA <89544645+its-as2002@users.noreply.github.com>
2024-08-12 13:33:27 +0530

    Insert scipt.js and update style.css and index.html
    built the logic in scipt.js for the calcultor and in style.css added hover feature and intro  css, and in html file added name of project

---
+++ script.js
@@ -0,0 +1,57 @@
+function calc() {
+    let operators = new Set(['+','-','/','*','%']);
+    let numbers = new Set(['1','2','3','4','5','6','7','8','9','0','.']);
+    let ans = '';
+    
+    document.querySelector('.buttons_border').addEventListener('click',function(details) {
+        let choice =  details.target.innerHTML;
+        let txt = document.querySelector('.textarea');
+        
+        switch(choice){
+                case 'AC':
+                    ans ='';
+                    txt.innerHTML = ans;
+                    break;
+                case '00':
+                    ans ='0';
+                    txt.innerHTML = ans;
+                    break;
+                case '⌫':
+                    ans= ans.slice(0,ans.length-1);
+                    txt.innerHTML = ans;
+                    break;
+                case '=':
+                    txt.innerHTML = eval(ans);
+                    ans = '';
+                    break;
+                
+                default:
+                    if (operators.has(choice)){
+
+                        if((ans.length === 0)){
+                            if((choice == '+' )|| (choice == '-')){
+                                ans+=choice;
+                                txt.innerHTML = ans;
+                            }
+                            else
+                                txt.innerHTML = 'error';
+                        }
+
+                        else if(operators.has(ans[ans.length -1])){
+                                ans = ans.slice(0,-1) + choice;
+                                txt.innerHTML = ans;
+                            }
+                        else{
+                                ans+=choice;
+                                txt.innerHTML = ans;
+                            }
+                    }
+                    else if(numbers.has(choice)){
+                        ans+=choice;
+                        txt.innerHTML = ans;
+                    }
+            }
+    })
+     
+}
+calc();
